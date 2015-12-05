5238392bc729fcf75fb3aa59fba1ff57ade1a7ec
Anuj <a_gujar@yahoo.com>
2015-12-04 22:15:22 -0800

    fix just for you restaurant list

--- ooApp/RestaurantListVC.m
+++ ooApp/RestaurantListVC.m
@@ -200,13 +200,13 @@ static NSString * const cellIdentifier = @"horizontalCell";
             ;
         }];
     } else {
-        self.requestOperation = [api getRestaurantsWithKeywords:@[_listItem.name]
+        self.requestOperation = [api getRestaurantsWithKeywords:(_listItem.type == kListTypeJustForYou) ? @[@"restaurants"]: @[_listItem.name]
                                                    andLocation:[[LocationManager sharedInstance] currentUserLocation]
                                                      andFilter:@""
                                                     andRadius:2000
                                                    andOpenOnly:NO
                                                        andSort:kSearchSortTypeBestMatch
-                                                         isPlay:NO
+                                                         isPlay:(_listItem.type == kListTypeJustForYou) ? YES : NO
                                                         success:^(NSArray *r) {
             weakSelf.restaurants = r;
             dispatch_async(dispatch_get_main_queue(), ^{
