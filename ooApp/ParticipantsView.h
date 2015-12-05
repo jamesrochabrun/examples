e623e67cf8a6918a72cf99b986289e740c3a7df7
Anuj <a_gujar@yahoo.com>
2015-12-04 22:05:08 -0800

    use OOUserView in eventTVCs

--- ooApp/ParticipantsView.h
+++ ooApp/ParticipantsView.h
@@ -1,10 +1,11 @@
 #import "EventObject.h"
+#import "OOUserView.h"
 
 @protocol ParticipantsViewDelegate
 - (void) userPressedButtonForProfile: (NSUInteger)userid;
 @end
 
-@interface ParticipantsView:UIView
+@interface ParticipantsView:UIView <OOUserViewDelegate>
 - (void)clearFaces;
 - (void) setEvent: (EventObject*)event;
 @property (nonatomic,weak) id <ParticipantsViewDelegate> delegate;
--- ooApp/ParticipantsView.m
+++ ooApp/ParticipantsView.m
@@ -1,21 +1,19 @@
 #import "ParticipantsView.h"
+#import "OOUserView.h"
 
 @interface ParticipantsView()
-@property (nonatomic,strong) EventObject*event;
-
-@property (nonatomic,strong) NSMutableArray* viewsForFaces;
-@property (nonatomic,strong)  UILabel* labelEllipsis;
+@property (nonatomic, strong) EventObject *event;
+@property (nonatomic, strong) NSMutableArray *viewsForFaces;
+@property (nonatomic, strong) UILabel *labelEllipsis;
 @end
 
 @implementation ParticipantsView
 
-- (instancetype) init
+- (instancetype)init
 {
     self = [super init];
     if (self) {
-        _labelEllipsis= makeLabel(self,  @"...", kGeomFontSizeHeader);
-
-//        self.backgroundColor= YELLOW;
+        _labelEllipsis = makeLabel(self,  @"...", kGeomFontSizeHeader);
     }
     return self;
 }
@@ -23,52 +21,75 @@
 - (void)dealloc
 {
     [_labelEllipsis removeFromSuperview];
-    self.labelEllipsis= nil;
+    self.labelEllipsis = nil;
     [self clearFaces];
-    self.viewsForFaces= nil;
+    self.viewsForFaces = nil;
 }
 
 - (void)clearFaces
 {
     if (_viewsForFaces ) {
-        for (UIView* v  in  _viewsForFaces) {
+        for (UIView *v in _viewsForFaces) {
             [v removeFromSuperview];
         }
         [self.viewsForFaces removeAllObjects];
     }
 }
 
-- (void) setEvent: (EventObject*)event
+- (void)setEvent:(EventObject *)event
 {
-    if  (!event) {
+    if (!event) {
         return;
     }
     
     _event= event;
     
-    if  ([self.event totalUsers ] ) {
+    if  ([self.event totalUsers]) {
         [self clearFaces];
       
-        float availableWidth= self.frame.size.width;
-        if  (!availableWidth) {
+        float availableWidth = self.frame.size.width;
+        if (!availableWidth) {
             return;
         }
-        NSInteger nBubbles= (availableWidth-2*kGeomSpaceEdge)/(kGeomFaceBubbleDiameter +kGeomFaceBubbleSpacing);
-        self.viewsForFaces= makeImageViewsForUsers (self, event.users, nBubbles,self,@selector(userPressedFaceBubble:) );
+        
+        NSInteger nBubbles = (availableWidth-2*kGeomSpaceEdge)/(kGeomFaceBubbleDiameter + kGeomFaceBubbleSpacing);
+//        self.viewsForFaces =  makeImageViewsForUsers (self, event.users, nBubbles,self,@selector(userPressedFaceBubble:) );
+        
+        self.viewsForFaces = [NSMutableArray array];
+        OOUserView *uv;
+        int i = 0;
+        for (UserObject *u in event.users) {
+            uv = [[OOUserView alloc] init];
+            uv.delegate = self;
+            [self addSubview:uv];
+            uv.frame = CGRectMake(0, 0, kGeomFaceBubbleDiameter, kGeomFaceBubbleDiameter);
+            uv.user = u;
+            [self.viewsForFaces addObject:uv];
+            i++;
+            if  (i >= nBubbles) {
+                break;
+            }
+        }
         [self setNeedsLayout];
+    }
+}
+
+- (void)oOUserViewTapped:(OOUserView *)userView forUser:(UserObject *)user {
+    if ( self.delegate) {
+        [self.delegate userPressedButtonForProfile:user.userID];
     }
 }
 
 - (void)layoutSubviews
 {
     if ( self.viewsForFaces.count) {
-        float w= self.frame.size.width;
+        float w = self.frame.size.width;
         NSUInteger count=self.viewsForFaces.count;
         NSUInteger totalPeople=  [self.event totalUsers ];
-        float y=kGeomSpaceEdge;
-        float x= (w-count*kGeomFaceBubbleDiameter-(count-1)*kGeomFaceBubbleSpacing)/2;
+        float y = kGeomSpaceEdge;
+        float x = (w-count*kGeomFaceBubbleDiameter-(count-1)*kGeomFaceBubbleSpacing)/2;
         NSInteger i= 0;
-        for (UIImageView*iv  in self.viewsForFaces) {
+        for (OOUserView*iv  in self.viewsForFaces) {
             if  (i >= _viewsForFaces.count-1  && _viewsForFaces.count < totalPeople  ) {
                 _labelEllipsis.frame=CGRectMake(x, y, kGeomFaceBubbleDiameter, kGeomFaceBubbleDiameter);
                 iv.frame= CGRectZero;
@@ -84,8 +105,8 @@
 
 - (void)userPressedFaceBubble: (UIButton*)button
 {
-    NSLog  (@"USER PRESSED FACE BUBBLE  %lu",(unsigned long)button.tag);
-    if ( self.delegate) {
+    NSLog(@"USER PRESSED FACE BUBBLE  %lu",(unsigned long)button.tag);
+    if (self.delegate) {
         [self.delegate userPressedButtonForProfile: button.tag ];
     }
 }
